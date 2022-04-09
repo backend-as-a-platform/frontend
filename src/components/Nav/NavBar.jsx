2c8b3bb8dba6d829b40ce2c79415212cd164d4d1
arjunshibu <arjunshibu1999@gmail.com>
2022-04-09 15:35:11 +0530

    fixed navbar z-index (modal overlay issue)

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -5,7 +5,7 @@ import { NavItems, NavItemsDropdown } from './NavItems';
 const Navbar = ({ toggleTheme, onSignup }) => {
   return (
     <div
-      className="navbar sticky top-0 z-50 bg-base-100 backdrop-blur lg:border-b dark:border-b-slate-50/[0.06] shadow-sm py-1"
+      className="navbar sticky top-0 z-10 bg-base-100 backdrop-blur lg:border-b dark:border-b-slate-50/[0.06] shadow-sm py-1"
       data-aos="zoom-in"
       data-aos-delay="300"
       data-aos-once="true"
