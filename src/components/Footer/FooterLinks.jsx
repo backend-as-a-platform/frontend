9e3b7d988aff7804cb895cb7e102f752ab7d5732
arjunshibu <arjunshibu1999@gmail.com>
2022-04-15 20:03:34 +0530

    added footer links

--- src/components/Footer/FooterLinks.jsx
+++ src/components/Footer/FooterLinks.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import {
   TwitterIcon,
   YoutubeIcon,
@@ -6,10 +7,18 @@ import {
 
 export const FooterLinks = () => (
   <div className="grid grid-flow-col gap-4">
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">FAQs</a>
-    <a className="link link-hover">Support</a>
+    <Link to="/about" className="link link-hover">
+      About us
+    </Link>
+    <Link to="/contact" className="link link-hover">
+      Contact
+    </Link>
+    <Link to="/faq" className="link link-hover">
+      FAQs
+    </Link>
+    <Link to="/support" className="link link-hover">
+      Support
+    </Link>
   </div>
 );
 
